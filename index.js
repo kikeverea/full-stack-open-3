d6be86a6795da6dc8d11aecede698ecf87d17513
Enrique Verea <kikeverea@gmail.com>
2022-07-22 13:15:02 +0200

    Step 2: /info => person count and time of request

--- index.js
+++ index.js
@@ -33,5 +33,12 @@ let persons = [
 app.get('/', (request, response) =>
   response.send('Hello World'))
 
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`
+  )}
+)
+
 app.get('/api/persons', (request, response) =>
   response.json(persons))
