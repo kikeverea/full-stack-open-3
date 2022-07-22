ae2b328cfb2c31e4e90b5a2e3a3759882a81c0b2
Enrique Verea <kikeverea@gmail.com>
2022-07-22 14:18:34 +0200

    Step 7: add 'morgan' middleware. Use 'tiny' config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { request, response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const PORT = 3001
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.listen(PORT)
 
