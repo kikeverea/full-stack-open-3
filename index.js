e1ec1d477516151b5cabba4af7e89b4121531be8
Enrique Verea <kikeverea@gmail.com>
2022-08-03 23:35:06 +0200

    Add ESLint. End of part 3

--- index.js
+++ index.js
@@ -2,46 +2,47 @@ const express = require('express')
 const morgan = require('morgan')
 const Person = require('./models/Person')
 
-morgan.token('body', (req, res) => {
+morgan.token('body', req => {
   const person = req.body
-  delete person.id 
+  delete person.id
   return JSON.stringify(person)
 })
 
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
 const app = express()
 const cors = require('cors')
-const PORT = process.env.PORT || 3001
 const morganFormat = ':method :url :status :res[content-length] - :response-time ms :body'
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 app.use(morgan(morganFormat, {
-  skip: (req, res) => req.method.toUpperCase() === 'POST'
+  skip: req => req.method.toUpperCase() === 'POST'
 }))
 app.use(morgan(morganFormat, {
-  skip: (req, res) => req.method.toUpperCase() !== 'POST'
+  skip: req => req.method.toUpperCase() !== 'POST'
 }))
 
 app.listen(PORT)
 
-app.get('/info', (request, response) => 
+app.get('/info', (request, response) =>
   Person.countDocuments({})
     .then(count => {
       response.send(
         `<p>Phonebook has info for ${count} people</p>
         <p>${new Date()}</p>`
       )
-    })  
+    })
 )
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => 
+  Person.find({}).then(persons =>
     response.json(persons)
   )
 })
 
-app.get('/api/persons/:id', (request, response, next) => {  
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -59,15 +60,15 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const body = prepareForSave(request.body)
 
-  Person.findOne({name: body.name})
+  Person.findOne({ name: body.name })
     .then(person => {
       if (person) {
         response
           .status(409)
           .json({
-            error: 
+            error:
               `Name must be unique. '${body.name}' is already in the phonebook`
-            })
+          })
       }
       else {
         const newPerson = new Person({
@@ -75,84 +76,73 @@ app.post('/api/persons', (request, response, next) => {
           number: body.number,
           date: new Date(),
         })
-      
+
         newPerson.save()
-        .then(savedPerson => {
-          response.status(201).json(savedPerson)
-        })
-        .catch(error => {
-          next(error)
-        })
+          .then(savedPerson => {
+            response.status(201).json(savedPerson)
+          })
+          .catch(error => {
+            next(error)
+          })
       }
     })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
- 
+
   const { name, number } = prepareForSave(request.body)
 
-  // mongoose model validation skips 'required' constraints in updates 
-  if (!name) 
+  // mongoose model validation skips 'required' constraints in updates
+  if (!name)
     return response.status(400).json({
-      error: 'Person validation failed: name: Name is required'})
+      error: 'Person validation failed: name: Name is required' })
 
-  if (!number) 
+  if (!number)
     return response.status(400).json({
-      error: 'Person validation failed: number: Number is required'})
+      error: 'Person validation failed: number: Number is required' })
 
   Person.findByIdAndUpdate(
     request.params.id,
     { name, number },
     { new: true, runValidators: true, context: 'query' }
   )
-  .then(updatedPerson => {
-    response.status(200).json(updatedPerson)
-  })
-  .catch(error => {
-    next(error)
-  })
+    .then(updatedPerson => {
+      response.status(200).json(updatedPerson)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => response.status(204).end())
+    .then(() => response.status(204).end())
 })
 
 const prepareForSave = (body) => {
   body.number = body.number
-    ? body.number.replace(/\s/g, "")
+    ? body.number.replace(/\s/g, '')
     : null
 
   return body
 }
 
 const errorHandler = (error, request, response, next) => {
-  console.log("ERROR", error)
+  console.log('ERROR', error)
 
   if (error.name === 'CastError') {
     return response
-          .status(400)
-          .send({error: 'Malformed id'})
+      .status(400)
+      .send({ error: 'Malformed id' })
   }
 
   if (error.name === 'ValidationError') {
     return response
-          .status(400)
-          .json({error: error.message})
+      .status(400)
+      .json({ error: error.message })
   }
 
   next(error)
 }
 
 app.use(errorHandler)
-
-
-
-
-
-
-
-
-
-
-
--- models/Person.js
+++ models/Person.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+// eslint-disable-next-line no-undef
 const url = process.env.PERSONS_DB_URI
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -12,23 +13,22 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  "id": String,
-  "name": {
-    type: String, 
+  'id': String,
+  'name': {
+    type: String,
     required: [true, 'Name is required'],
     minLength: [3, 'Name must be at least 3 characters long']
-  }, 
-  "number": {
-    type: String, 
+  },
+  'number': {
+    type: String,
     required: [true, 'Number is required'],
     minLength: [8, 'Number must be at least 8 characters long'],
     validate: {
       validator: value => {
-        console.log("VALIDATING...")
         return value.match('^[0-9]{2,3}[-]?[0-9]+(?<=.{8,})$')
       },
-      message: "Phone number characters can only be separated by a single " + 
-                "hyphen (-), after the 2nd or 3rd character"
+      message: 'Phone number characters can only be separated by a single \
+                hyphen (-), after the second or third character'
     }
   }
 })
